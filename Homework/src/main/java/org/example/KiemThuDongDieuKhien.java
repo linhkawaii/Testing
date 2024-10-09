8c565f7607da17468ccc9c1efe5d4f2c4448a0b2
NguyenLinh <118706598+nguyenlinhyuri@users.noreply.github.com>
2024-10-09 10:01:13 +0700

    upcode

---
+++ Homework/src/main/java/org/example/KiemThuDongDieuKhien.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class KiemThuDongDieuKhien {
+    public static boolean ValidOrInvalid(String ID) {
+        // input invalid
+        if (ID == null || ID.length() != 8){
+            return false;
+        }
+        for (int i=0; i<ID.length(); i++) {
+            if (ID.charAt(i) < '0' || ID.charAt(i) > '9'){
+                return false;
+            }
+        }
+        return true;
+    }
+}
